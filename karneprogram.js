e75d96022b543c3471a6e1cf8e3b86174ddbc5f6
ceymismontgibi <48599030+ceymismontgibi@users.noreply.github.com>
2019-03-20 20:53:41 +0300

    Add files via upload

---
+++ karneprogram.js
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="tr">
+    <head>
+	<title>JavaScript,HTML5,CSS denemeleri</title>
+	<meta charset="utf-8">
+	</head>
+    <body>
+
+	</body>
+	<script>
+    
+    not=Number(prompt("Öğrencinin notunu giriniz."));
+    
+    if (not<55)
+       alert("Kaldı")
+    else if (not<75)
+       alert("Orta")
+    else if (not<84)
+       alert("İyi")
+    else 
+       alert("Pek İyi")
+   
+
+
+
+
+	</script>
+</html>
