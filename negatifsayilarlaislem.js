28b28db6fb23e35ac922925e01776738adf09d8c
ceymismontgibi <48599030+ceymismontgibi@users.noreply.github.com>
2019-03-20 20:52:52 +0300

    Add files via upload

---
+++ negatifsayilarlaislem.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="utf-8">
+        <title>JS, CSS, HTML5</title>
+    </head>
+    <body>
+
+    </body>
+    <script>
+    sayi1=Number(prompt("Sayı 1: "));
+    sayi2=Number(prompt("Sayı 2: "));
+
+    if (sayi1>0)
+    {
+        alert("Pozitif sayılarla işlem yapılmamaktadır.");
+        alert("Lütfen F5 ile sayfanızı yenileyiniz.");
+    }
+    else
+    {
+        toplam=sayi1+sayi2
+        alert("Toplam= "+toplam);
+    }
+
+    
+
+    </script>
+</html>
